569b688808b91a98ccd36b8c69d44c60b6d99365
luciarp10 <lucia.rodriguezperez@edu.uah.es>
2021-11-30 16:38:56 +0100

    eliminación de public. en todas las instrucciones

--- LMDL - BD/codigo_SQL.sql
+++ LMDL - BD/codigo_SQL.sql
@@ -13,9 +13,9 @@ CREATE DATABASE new_database;
 -- ddl-end --
 
 
--- object: public.habitacion | type: TABLE --
--- DROP TABLE IF EXISTS public.habitacion CASCADE;
-CREATE TABLE public.habitacion (
+-- object: habitacion | type: TABLE --
+-- DROP TABLE IF EXISTS habitacion CASCADE;
+CREATE TABLE habitacion (
 	id_habitacion integer NOT NULL,
 	n_puertas integer NOT NULL,
 	n_ventanas integer NOT NULL,
@@ -25,183 +25,184 @@ CREATE TABLE public.habitacion (
 
 );
 -- ddl-end --
-ALTER TABLE public.habitacion OWNER TO postgres;
+ALTER TABLE habitacion OWNER TO postgres;
 -- ddl-end --
 
--- object: public.identificacion | type: TABLE --
--- DROP TABLE IF EXISTS public.identificacion CASCADE;
-CREATE TABLE public.identificacion (
+-- object: identificacion | type: TABLE --
+-- DROP TABLE IF EXISTS identificacion CASCADE;
+CREATE TABLE identificacion (
 	codigo_qr integer NOT NULL,
-	password integer NOT NULL,
-	nombre text NOT NULL,
+	password char(20) NOT NULL,
+	nombre char(50) NOT NULL,
 	cod_sistema_sistema_seguridad integer,
 	CONSTRAINT identificacion_pk PRIMARY KEY (nombre)
 
 );
 -- ddl-end --
-ALTER TABLE public.identificacion OWNER TO postgres;
+ALTER TABLE identificacion OWNER TO postgres;
 -- ddl-end --
 
--- object: public.registro_estadistico | type: TABLE --
--- DROP TABLE IF EXISTS public.registro_estadistico CASCADE;
-CREATE TABLE public.registro_estadistico (
-	fecha_user date,
-	valor_user float,
-	hora_user float,
+-- object: registro_estadistico | type: TABLE --
+-- DROP TABLE IF EXISTS registro_estadistico CASCADE;
+CREATE TABLE registro_estadistico (
+	fecha date NOT NULL,
+	valor float NOT NULL,
+	hora time NOT NULL,
 	id_sensor_sensor integer
 );
 -- ddl-end --
-ALTER TABLE public.registro_estadistico OWNER TO postgres;
+ALTER TABLE registro_estadistico OWNER TO postgres;
 -- ddl-end --
 
--- object: public.registro_camara | type: TABLE --
--- DROP TABLE IF EXISTS public.registro_camara CASCADE;
-CREATE TABLE public.registro_camara (
-	fecha date,
-	foto bit,
-	hora float,
+-- object: registro_camara | type: TABLE --
+-- DROP TABLE IF EXISTS registro_camara CASCADE;
+CREATE TABLE registro_camara (
+	fecha date NOT NULL,
+	foto character(256) NOT NULL,
+	hora time NOT NULL,
 	id_sensor_sensor integer
 );
 -- ddl-end --
-ALTER TABLE public.registro_camara OWNER TO postgres;
+ALTER TABLE registro_camara OWNER TO postgres;
 -- ddl-end --
 
--- object: public.cliente | type: TABLE --
--- DROP TABLE IF EXISTS public.cliente CASCADE;
-CREATE TABLE public.cliente (
-	dni text NOT NULL,
+-- object: cliente | type: TABLE --
+-- DROP TABLE IF EXISTS cliente CASCADE;
+CREATE TABLE cliente (
+	dni char(9) NOT NULL,
 	id_cliente integer NOT NULL,
-	direccion character(50) NOT NULL,
-	telefono integer,
-	nombre text NOT NULL,
+	direccion char(50) NOT NULL,
+	telefono char(9) NOT NULL,
+	nombre char(50) NOT NULL,
 	CONSTRAINT id_cliente PRIMARY KEY (id_cliente)
 
 );
 -- ddl-end --
-ALTER TABLE public.cliente OWNER TO postgres;
+ALTER TABLE cliente OWNER TO postgres;
 -- ddl-end --
 
--- object: public.registro | type: TABLE --
--- DROP TABLE IF EXISTS public.registro CASCADE;
-CREATE TABLE public.registro (
-	hora_on float,
-	fecha_on date,
-	duracion float,
+-- object: registro | type: TABLE --
+-- DROP TABLE IF EXISTS registro CASCADE;
+CREATE TABLE registro (
+	hora_on time NOT NULL,
+	fecha_on date NOT NULL,
+	duracion float NOT NULL,
 	id_actuador_actuador integer
 );
 -- ddl-end --
-ALTER TABLE public.registro OWNER TO postgres;
+ALTER TABLE registro OWNER TO postgres;
 -- ddl-end --
 
--- object: public.sistema_seguridad | type: TABLE --
--- DROP TABLE IF EXISTS public.sistema_seguridad CASCADE;
-CREATE TABLE public.sistema_seguridad (
+-- object: sistema_seguridad | type: TABLE --
+-- DROP TABLE IF EXISTS sistema_seguridad CASCADE;
+CREATE TABLE sistema_seguridad (
 	cod_sistema integer NOT NULL,
-	direccion character(50) NOT NULL,
-	nombre text NOT NULL,
+	direccion char(50) NOT NULL,
+	nombre char(50) NOT NULL,
+	contrasenna_acceso char(4) NOT NULL,
 	id_cliente_cliente integer,
 	CONSTRAINT sistema_seguridad_pk PRIMARY KEY (cod_sistema)
 
 );
 -- ddl-end --
-ALTER TABLE public.sistema_seguridad OWNER TO postgres;
+ALTER TABLE sistema_seguridad OWNER TO postgres;
 -- ddl-end --
 
 -- object: cliente_fk | type: CONSTRAINT --
--- ALTER TABLE public.sistema_seguridad DROP CONSTRAINT IF EXISTS cliente_fk CASCADE;
-ALTER TABLE public.sistema_seguridad ADD CONSTRAINT cliente_fk FOREIGN KEY (id_cliente_cliente)
-REFERENCES public.cliente (id_cliente) MATCH FULL
+-- ALTER TABLE sistema_seguridad DROP CONSTRAINT IF EXISTS cliente_fk CASCADE;
+ALTER TABLE sistema_seguridad ADD CONSTRAINT cliente_fk FOREIGN KEY (id_cliente_cliente)
+REFERENCES cliente (id_cliente) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: sistema_seguridad_fk | type: CONSTRAINT --
--- ALTER TABLE public.identificacion DROP CONSTRAINT IF EXISTS sistema_seguridad_fk CASCADE;
-ALTER TABLE public.identificacion ADD CONSTRAINT sistema_seguridad_fk FOREIGN KEY (cod_sistema_sistema_seguridad)
-REFERENCES public.sistema_seguridad (cod_sistema) MATCH FULL
+-- ALTER TABLE identificacion DROP CONSTRAINT IF EXISTS sistema_seguridad_fk CASCADE;
+ALTER TABLE identificacion ADD CONSTRAINT sistema_seguridad_fk FOREIGN KEY (cod_sistema_sistema_seguridad)
+REFERENCES sistema_seguridad (cod_sistema) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: sistema_seguridad_fk | type: CONSTRAINT --
--- ALTER TABLE public.habitacion DROP CONSTRAINT IF EXISTS sistema_seguridad_fk CASCADE;
-ALTER TABLE public.habitacion ADD CONSTRAINT sistema_seguridad_fk FOREIGN KEY (cod_sistema_sistema_seguridad)
-REFERENCES public.sistema_seguridad (cod_sistema) MATCH FULL
+-- ALTER TABLE habitacion DROP CONSTRAINT IF EXISTS sistema_seguridad_fk CASCADE;
+ALTER TABLE habitacion ADD CONSTRAINT sistema_seguridad_fk FOREIGN KEY (cod_sistema_sistema_seguridad)
+REFERENCES sistema_seguridad (cod_sistema) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
--- object: public.sensor | type: TABLE --
--- DROP TABLE IF EXISTS public.sensor CASCADE;
-CREATE TABLE public.sensor (
+-- object: sensor | type: TABLE --
+-- DROP TABLE IF EXISTS sensor CASCADE;
+CREATE TABLE sensor (
 	id_sensor integer NOT NULL,
-	tipo text NOT NULL,
+	tipo char(50) NOT NULL,
 	id_habitacion_habitacion integer,
 	CONSTRAINT sensor_pk PRIMARY KEY (id_sensor)
 
 );
 -- ddl-end --
-ALTER TABLE public.sensor OWNER TO postgres;
+ALTER TABLE sensor OWNER TO postgres;
 -- ddl-end --
 
--- object: public.actuador | type: TABLE --
--- DROP TABLE IF EXISTS public.actuador CASCADE;
-CREATE TABLE public.actuador (
+-- object: actuador | type: TABLE --
+-- DROP TABLE IF EXISTS actuador CASCADE;
+CREATE TABLE actuador (
 	id_actuador integer NOT NULL,
-	tipo text NOT NULL,
+	tipo char(50) NOT NULL,
 	id_habitacion_habitacion integer,
 	CONSTRAINT actuador_pk PRIMARY KEY (id_actuador)
 
 );
 -- ddl-end --
-ALTER TABLE public.actuador OWNER TO postgres;
+ALTER TABLE actuador OWNER TO postgres;
 -- ddl-end --
 
 -- object: habitacion_fk | type: CONSTRAINT --
--- ALTER TABLE public.sensor DROP CONSTRAINT IF EXISTS habitacion_fk CASCADE;
-ALTER TABLE public.sensor ADD CONSTRAINT habitacion_fk FOREIGN KEY (id_habitacion_habitacion)
-REFERENCES public.habitacion (id_habitacion) MATCH FULL
+-- ALTER TABLE sensor DROP CONSTRAINT IF EXISTS habitacion_fk CASCADE;
+ALTER TABLE sensor ADD CONSTRAINT habitacion_fk FOREIGN KEY (id_habitacion_habitacion)
+REFERENCES habitacion (id_habitacion) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: habitacion_fk | type: CONSTRAINT --
--- ALTER TABLE public.actuador DROP CONSTRAINT IF EXISTS habitacion_fk CASCADE;
-ALTER TABLE public.actuador ADD CONSTRAINT habitacion_fk FOREIGN KEY (id_habitacion_habitacion)
-REFERENCES public.habitacion (id_habitacion) MATCH FULL
+-- ALTER TABLE actuador DROP CONSTRAINT IF EXISTS habitacion_fk CASCADE;
+ALTER TABLE actuador ADD CONSTRAINT habitacion_fk FOREIGN KEY (id_habitacion_habitacion)
+REFERENCES habitacion (id_habitacion) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: actuador_fk | type: CONSTRAINT --
--- ALTER TABLE public.registro DROP CONSTRAINT IF EXISTS actuador_fk CASCADE;
-ALTER TABLE public.registro ADD CONSTRAINT actuador_fk FOREIGN KEY (id_actuador_actuador)
-REFERENCES public.actuador (id_actuador) MATCH FULL
+-- ALTER TABLE registro DROP CONSTRAINT IF EXISTS actuador_fk CASCADE;
+ALTER TABLE registro ADD CONSTRAINT actuador_fk FOREIGN KEY (id_actuador_actuador)
+REFERENCES actuador (id_actuador) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: registro_uq | type: CONSTRAINT --
--- ALTER TABLE public.registro DROP CONSTRAINT IF EXISTS registro_uq CASCADE;
-ALTER TABLE public.registro ADD CONSTRAINT registro_uq UNIQUE (id_actuador_actuador);
+-- ALTER TABLE registro DROP CONSTRAINT IF EXISTS registro_uq CASCADE;
+ALTER TABLE registro ADD CONSTRAINT registro_uq UNIQUE (id_actuador_actuador);
 -- ddl-end --
 
 -- object: sensor_fk | type: CONSTRAINT --
--- ALTER TABLE public.registro_estadistico DROP CONSTRAINT IF EXISTS sensor_fk CASCADE;
-ALTER TABLE public.registro_estadistico ADD CONSTRAINT sensor_fk FOREIGN KEY (id_sensor_sensor)
-REFERENCES public.sensor (id_sensor) MATCH FULL
+-- ALTER TABLE registro_estadistico DROP CONSTRAINT IF EXISTS sensor_fk CASCADE;
+ALTER TABLE registro_estadistico ADD CONSTRAINT sensor_fk FOREIGN KEY (id_sensor_sensor)
+REFERENCES sensor (id_sensor) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: registro_estadistico_uq | type: CONSTRAINT --
--- ALTER TABLE public.registro_estadistico DROP CONSTRAINT IF EXISTS registro_estadistico_uq CASCADE;
-ALTER TABLE public.registro_estadistico ADD CONSTRAINT registro_estadistico_uq UNIQUE (id_sensor_sensor);
+-- ALTER TABLE registro_estadistico DROP CONSTRAINT IF EXISTS registro_estadistico_uq CASCADE;
+ALTER TABLE registro_estadistico ADD CONSTRAINT registro_estadistico_uq UNIQUE (id_sensor_sensor);
 -- ddl-end --
 
 -- object: sensor_fk | type: CONSTRAINT --
--- ALTER TABLE public.registro_camara DROP CONSTRAINT IF EXISTS sensor_fk CASCADE;
-ALTER TABLE public.registro_camara ADD CONSTRAINT sensor_fk FOREIGN KEY (id_sensor_sensor)
-REFERENCES public.sensor (id_sensor) MATCH FULL
+-- ALTER TABLE registro_camara DROP CONSTRAINT IF EXISTS sensor_fk CASCADE;
+ALTER TABLE registro_camara ADD CONSTRAINT sensor_fk FOREIGN KEY (id_sensor_sensor)
+REFERENCES sensor (id_sensor) MATCH FULL
 ON DELETE SET NULL ON UPDATE CASCADE;
 -- ddl-end --
 
 -- object: registro_camara_uq | type: CONSTRAINT --
--- ALTER TABLE public.registro_camara DROP CONSTRAINT IF EXISTS registro_camara_uq CASCADE;
-ALTER TABLE public.registro_camara ADD CONSTRAINT registro_camara_uq UNIQUE (id_sensor_sensor);
+-- ALTER TABLE registro_camara DROP CONSTRAINT IF EXISTS registro_camara_uq CASCADE;
+ALTER TABLE registro_camara ADD CONSTRAINT registro_camara_uq UNIQUE (id_sensor_sensor);
 -- ddl-end --
 
 
