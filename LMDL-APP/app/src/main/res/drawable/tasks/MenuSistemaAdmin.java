5c35a55bd3e3532b82ff9d4299ef6e0c7c4cada6
davidBarreiro <david.barreiro@edu.uah.es>
2022-01-04 11:27:05 +0100

    Actividad encargada del menu de un admin

---
+++ LMDL-APP/app/src/main/res/drawable/tasks/MenuSistemaAdmin.java
@@ -0,0 +1,67 @@
+package com.example.lmdl_app.tasks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lmdl_app.R;
+
+public class MenuSistemaAdmin extends AppCompatActivity {
+
+    private Button botonHabitaciones;
+    private Button botonSeguridad;
+    private Button botonEstadisticas;
+    private Button botonAdminUsers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_0_1_menu_admi);
+
+        //Inicializamos botones y spinners
+        this.botonHabitaciones = this.findViewById(R.id.botonHab);
+        this.botonSeguridad = this.findViewById(R.id.botonSeg);
+        this.botonEstadisticas = this.findViewById(R.id.botonEst);
+        this.botonAdminUsers = this.findViewById(R.id.botonAdminUser);
+
+        botonHabitaciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuSistemaAdmin.this, Habitaciones.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        botonSeguridad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuSistemaAdmin.this, Seguridad.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        botonEstadisticas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuSistemaAdmin.this, Estadisticas.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        botonAdminUsers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuSistemaAdmin.this, Usuarios.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+    }
+}
