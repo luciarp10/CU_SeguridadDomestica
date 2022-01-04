f74b34f6f9ba6817a5c41780f719bb2282675379
davidBarreiro <david.barreiro@edu.uah.es>
2022-01-04 11:28:40 +0100

    Actividad encargada de los registros de las camaras

---
+++ LMDL-APP/app/src/main/res/drawable/tasks/RegistrosCamaras.java
@@ -0,0 +1,16 @@
+package com.example.lmdl_app.tasks;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lmdl_app.R;
+
+public class RegistrosCamaras extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2_2_1_camaras);
+    }
+}
