9eeda983c926d3ec253a8aeea410a9e0b622ce4a
davidBarreiro <david.barreiro@edu.uah.es>
2022-01-04 11:25:52 +0100

    Actividad encargada de los informes

---
+++ LMDL-APP/app/src/main/res/drawable/tasks/Informes.java
@@ -0,0 +1,16 @@
+package com.example.lmdl_app.tasks;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lmdl_app.R;
+
+public class Informes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3_1_informes);
+    }
+}
