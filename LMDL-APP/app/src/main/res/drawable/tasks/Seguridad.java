65cbc26989dd68bad942c5cd589b5da14bca7894
davidBarreiro <david.barreiro@edu.uah.es>
2022-01-04 11:28:54 +0100

    Actividad encargada de las opciones de seguridad

---
+++ LMDL-APP/app/src/main/res/drawable/tasks/Seguridad.java
@@ -0,0 +1,77 @@
+package com.example.lmdl_app.tasks;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.lmdl_app.R;
+
+
+public class Seguridad extends AppCompatActivity {
+
+    private Button botonRegistros;
+    private Button botonCamaras;
+    private Button botonSimuladorPres;
+    private Button botonEstadoAlarma;
+    private Button botonVerQR;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2_seguridad);
+
+        this.botonRegistros = this.findViewById(R.id.botonReg);
+        this.botonCamaras = this.findViewById(R.id.botonCam);
+        this.botonSimuladorPres = this.findViewById(R.id.botonSimPres);
+        this.botonEstadoAlarma = this.findViewById(R.id.botonEstAlar);
+        this.botonVerQR = this.findViewById(R.id.botonMiQR);
+
+        botonRegistros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Seguridad.this, Registros.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        botonCamaras.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Seguridad.this, Camaras.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        botonSimuladorPres.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Seguridad.this, SimulacionPresencia.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        botonEstadoAlarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Seguridad.this, EstadoAlarma.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        botonVerQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Seguridad.this, VerQR.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+}
