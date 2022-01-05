45fe4327cf45ee6f5411d4e53c6389b85c4d0370
luciarp10 <lucia.rodriguezperez@edu.uah.es>
2022-01-06 00:56:56 +0100

    Actividad de estadisticas de registros

--- LMDL-APP/app/src/main/java/com/example/lmdl_app/MenuSistema.java
+++ LMDL-APP/app/src/main/java/com/example/lmdl_app/MenuSistema.java
@@ -1,20 +1,63 @@
 package com.example.lmdl_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.lmdl_app.R;
 
 public class MenuSistema  extends AppCompatActivity {
+    private Button botonHabitaciones;
+    private Button botonSeguridad;
+    private Button botonEstadisticas;
 
-
+    private String codigoSist = ""; //parametro que pasa main
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_0_menu);
+
+        //Inicializamos botones y spinners
+        this.botonHabitaciones = this.findViewById(R.id.botonHabi);
+        this.botonSeguridad = this.findViewById(R.id.botonSeguri);
+        this.botonEstadisticas = this.findViewById(R.id.botonEstadi);
+
+        codigoSist = getIntent().getStringExtra("cod_sistema");
+
+        botonHabitaciones.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuSistema.this, Habitaciones.class);
+                i.putExtra("cod_sistema", codigoSist); //Se pasa el codigo de sistema al resto de actividades
+                startActivity(i);
+
+                //finish();
+            }
+        });
+
+        botonSeguridad.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuSistema.this, Seguridad.class);
+                startActivity(i);
+                //finish();
+            }
+        });
+
+        botonEstadisticas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MenuSistema.this, Estadisticas.class);
+                startActivity(i);
+                //finish();
+            }
+        });
+
 
 
     }
