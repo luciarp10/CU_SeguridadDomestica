352b007c422e500dc7644de7e23af2815c9abfae
davidBarreiro <david.barreiro@edu.uah.es>
2022-01-05 01:22:53 +0100

    Cambio en actividad Habitaciones para hacer las primeras llamadas al servidor

--- LMDL-APP/app/src/main/java/com/example/lmdl_app/Habitaciones.java
+++ LMDL-APP/app/src/main/java/com/example/lmdl_app/Habitaciones.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
@@ -14,128 +15,99 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.lmdl_app.data.Habitacion;
 import com.example.lmdl_app.tasks.TaskSelectHabitacion;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class Habitaciones extends AppCompatActivity {
-
-    /*
-    private String tag = "SelectStation";
-    private Spinner spinnerCities;
-    private Spinner spinnerStations;
-    private Button buttonStation;
-    private ArrayList<City> listCities;
-    private ArrayList<Station> listStation;
-    private final Context context;
-    private int idStation = 0;
-    private String nameStation = "";*/
 
     private String tag = "SelectHabitacion";
     private Button botonEstadisticas;
     private Spinner spinnerHabitac;
     private int idHab = 0;
     private String nameHab = "";
+    ArrayList<String> arrayHab;
     private ArrayList<Habitacion> listHabitacion;
-    //private final Context context;
+    private final Context context;
+
+    private String codigoSist = ""; //parametro que pasa main
+
+    public Habitaciones() {
+        super();
+        this.context = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_1_habitaciones);
 
-        this.botonEstadisticas = this.findViewById(R.id.botonEstadist);
-        this.spinnerHabitac = this.findViewById(R.id.spinnerHabitac);
-        //this.spinnerCities = this.findViewById(R.id.spinnerCity);
+        codigoSist = getIntent().getStringExtra("cod_sistema");
 
-        //this.listCities = new ArrayList<>();
+        this.botonEstadisticas = this.findViewById(R.id.botonEstadist);
+
         this.listHabitacion = new ArrayList<>();
 
         spinnerHabitac.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 int id = listHabitacion.get(i).getId_habitacion();//Get the id of the selected position
-                Log.i(tag, "City selected:" + listHabitacion.get(i).getDescriptivo());
+                Log.i(tag, "Habitacion selected:" + listHabitacion.get(i).getDescriptivo());
 
-                //Get the list of stations of the selected city
-                //loadHabitaciones(listHabitacion.get(i).getId_habitacion());
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
-        /*
-        * //Add action when the spinner of the cities changes
-        spinnerCities.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
-        {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                int id = listCities.get(i).getId();//Get the id of the selected position
-                Log.i(tag, "City selected:" + listCities.get(i).getName());
 
-                //Get the list of stations of the selected city
-                loadStations(listCities.get(i).getId());
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
-            }
-        });*/
-
-        /*
-        * buttonStation.setOnClickListener( new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(SelectStation.this, StationActivity.class);
-                i.putExtra("stationId", "station" + idStation);
-                i.putExtra("stationName", nameStation);
-                startActivity(i);
-                finish();
-            }
-        });*/
 
         botonEstadisticas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(Habitaciones.this, Estadisticas.class);
+                //i.putExtra("stationId", "station" + idStation);
+                //i.putExtra("stationName", nameStation);
                 startActivity(i);
                 //finish();
             }
         });
-
-        //loadCities();
         loadHabitaciones();
     }
 
     //Search the cities and fill the spinner with the information
     private void loadHabitaciones() {
         new TaskSelectHabitacion(this).
-                execute("http://192.168.1.108:8080/LMDL_SERVER2/GetHabitaciones?");
-              //execute("http://192.168.1.108:8080/LMDL_SERVER2/ComprobarQR?codigo="+codigoQR+"&id_sistema="+1);
-    }
-    /*
-    * //Search the cities and fill the spinner with the information
-    private void loadCities()
-    {
-        new TaskSelectStation(this).execute("http://192.168.1.131:8080/UbicompServerExample/GetCities");
+                execute("http://192.168.1.108:8080/LMDL_SERVER2/GetHabitacionesSistema?cod_sistema="+codigoSist);
+              //execute("http://192.168.1.108:8080/LMDL_SERVER2/
+             // ComprobarQR?codigo="+codigoQR+"&id_sistema="+1);
+
     }
 
-
-    public void setListCities(JSONArray jsonCities)
+    public void setListHabitacion(JSONArray jsonHabitaciones)
     {
-        Log.e(tag,"Loading cities " + jsonCities);
+        Log.e(tag,"Loading habitaciones " + jsonHabitaciones);
         try {
             ArrayList<String> arrayCities = new ArrayList<>();
             //listStation = new ArrayList<>();
-            for (int i = 0; i < jsonCities.length(); i++) {
-                JSONObject jsonobject = jsonCities.getJSONObject(i);
-                listCities.add(new City(jsonobject.getInt("id"), jsonobject.getString("name")));
-                arrayCities.add(jsonobject.getString("name"));
+            for (int i = 0; i < jsonHabitaciones.length(); i++) {
+                JSONObject jsonobject = jsonHabitaciones.getJSONObject(i);
+                listHabitacion.add(new Habitacion(jsonobject.getInt("id_habitacion"),
+                        jsonobject.getInt("n_puertas"),jsonobject.getInt("n_ventanas"),
+                        jsonobject.getInt("tamanno"),jsonobject.getInt("cod_sistema_sistema_seguridad"),
+                        jsonobject.getString("descriptivo")));
+                arrayHab.add(jsonobject.getString("descriptivo"));
+
             }
 
-            spinnerCities.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, arrayCities));
+            spinnerHabitac.setAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item, arrayHab));
         }catch (Exception e)
         {
             Log.e(tag,"Error: " + e);
         }
-    }*/
+
+    }
+
 }
+
