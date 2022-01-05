2a1de2bc4ad3527e1cc34d230f9d971f31942474
davidBarreiro <david.barreiro@edu.uah.es>
2022-01-05 12:49:43 +0100

    se crea la activida nuevo usuario

---
+++ LMDL-APP/app/src/main/java/com/example/lmdl_app/NuevoUsuario.java
@@ -0,0 +1,14 @@
+package com.example.lmdl_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NuevoUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_4_1_new_user);
+    }
+}
