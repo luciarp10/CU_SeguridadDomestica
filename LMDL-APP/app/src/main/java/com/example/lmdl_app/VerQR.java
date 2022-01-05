360e27374c2132c7589ade9ebdd52ffa1051ac6b
davidBarreiro <david.barreiro@edu.uah.es>
2022-01-06 00:33:17 +0100

    Se modifica actividad VerQr

--- LMDL-APP/app/src/main/java/com/example/lmdl_app/VerQR.java
+++ LMDL-APP/app/src/main/java/com/example/lmdl_app/VerQR.java
@@ -1,16 +1,42 @@
 package com.example.lmdl_app;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.lmdl_app.R;
+import com.google.zxing.BarcodeFormat;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 public class VerQR extends AppCompatActivity {
 
+    private ImageView qR;
+    private String textQr = "";
+    private int codigo_qr = 1234567; //Codigo recibido en json
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2_5_qr);
+
+        //Se enlaza la imagen del layout
+        qR = findViewById(R.id.imgQR);
+
+        //Se genera valor random
+        int random = (int)(Math.random()*999999999);
+
+        //Texto a generar como QR
+        textQr = ""+codigo_qr+random;
+
+        //Se genera QR
+        try {
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.encodeBitmap(textQr, BarcodeFormat.QR_CODE, 750, 750);
+            qR.setImageBitmap(bitmap);
+        } catch(Exception e) {
+
+        }
     }
+
 }
