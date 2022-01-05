cd64f6f9dba6974dcda34e497a41f862073636f8
davidBarreiro <david.barreiro@edu.uah.es>
2022-01-05 19:09:19 +0100

    Se crea Accion para boton añadir user

--- LMDL-APP/app/src/main/java/com/example/lmdl_app/Usuarios.java
+++ LMDL-APP/app/src/main/java/com/example/lmdl_app/Usuarios.java
@@ -1,6 +1,9 @@
 package com.example.lmdl_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +11,22 @@ import com.example.lmdl_app.R;
 
 public class Usuarios extends AppCompatActivity {
 
+    private Button botonCreaUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_4_usuarios);
+
+        this.botonCreaUser = this.findViewById(R.id.botonAñadirU);
+
+        botonCreaUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Usuarios.this, NuevoUsuario.class);
+                startActivity(i);
+                //finish();
+            }
+        });
     }
 }
