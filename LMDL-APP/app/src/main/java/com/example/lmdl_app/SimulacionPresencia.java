19710081b1691148f742f51f957694120dcee088
luciarp10 <lucia.rodriguezperez@edu.uah.es>
2022-01-06 19:34:32 +0100

    Activity mostrar registros de simulaciones en tabla

--- LMDL-APP/app/src/main/java/com/example/lmdl_app/SimulacionPresencia.java
+++ LMDL-APP/app/src/main/java/com/example/lmdl_app/SimulacionPresencia.java
@@ -11,12 +11,16 @@ public class SimulacionPresencia extends AppCompatActivity {
 
     private Button botonVerReg;
     private Button botonProgSim;
+    private String cod_sistema = ""; //Parametro que recibe de menuSistema o menuSistemaAdmin
+    private String usuariologin = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2_3_sim_pres);
 
+        usuariologin = getIntent().getStringExtra("usuario");
+        cod_sistema = getIntent().getStringExtra("cod_sistema");
         this.botonVerReg = this.findViewById(R.id.botonVerRegSim);
         this.botonProgSim = this.findViewById(R.id.botonProgSim);
 
@@ -24,6 +28,8 @@ public class SimulacionPresencia extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(SimulacionPresencia.this, SimulacionesProgramadas.class);
+                i.putExtra("cod_sistema", cod_sistema );
+                i.putExtra("usuario", usuariologin);
                 startActivity(i);
                 //finish();
             }
@@ -33,6 +39,8 @@ public class SimulacionPresencia extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(SimulacionPresencia.this, RegistrarSimulacion.class);
+                i.putExtra("cod_sistema", cod_sistema );
+                i.putExtra("usuario", usuariologin);
                 startActivity(i);
                 //finish();
             }
